2ccadc292a2214fa565cbab15a9ef737737a454c
Jeeyong Um <conr2d@proton.me>
2025-01-30 17:16:19 +0900

    chore: Update copyright symbol in license header

--- src/tests/test_deterministic_kat.rs
+++ src/tests/test_deterministic_kat.rs
@@ -1,4 +1,4 @@
-// Copyright (c) Jeeyong Um <conr2d@proton.me>
+// Copyright (C) Jeeyong Um <conr2d@proton.me>
 // SPDX-License-Identifier: MIT OR Apache-2.0
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
